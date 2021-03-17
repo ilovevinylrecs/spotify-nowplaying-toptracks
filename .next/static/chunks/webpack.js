311d2ab217402f9bc890e34d0bafb77dd8a3683f
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-03-16 22:28:30 -0400

    cleans up/removed unneeded files from app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e861c298e572a88e50b4";
+/******/ 	var hotCurrentHash = "d3939673bb17502f23b5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
