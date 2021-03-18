0ef9d082a9e9a828dc0b76bba8cf6d8d15b7162d
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-03-18 18:03:37 -0400

    cleans up .env.local file

--- pages/api/spotify.js
+++ pages/api/spotify.js
@@ -8,7 +8,6 @@ const {
 
 const basic = Buffer.from(`${client_id}:${client_secret}`).toString('base64');
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
-const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
@@ -35,17 +34,6 @@ export const getNowPlaying = async () => {
             Authorization: `Bearer ${access_token}`,
         },
     });
-};
-
-export const getTopTracks = async () => {
-    const { access_token } = await getAccessToken();
-
-    return fetch(TOP_TRACKS_ENDPOINT, {
-        headers: {
-            Authorization: `Bearer $(access_token)`,
-        }
-    });
-
 };
 
 export default async (_, res) => {
