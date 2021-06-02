ef95f55195f1d4e7761dacbcbf6949c1a0ba147f
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-06-02 15:01:27 -0400

    adds not listening functionality to footer component

--- components/now-playing.js
+++ components/now-playing.js
@@ -13,11 +13,54 @@ export default function NowPlayingModule() {
     fetchTrack();
   }, [])
 
-//   if (nowPlaying.length === 0) return 'currently listening to vinyl'
+  if (nowPlaying.length === 0) return 'lost connection to Spotify API'
 
   return (
     <center>
-    <div className="container">
+      { nowPlaying.isPlaying === false 
+        ? <div className="container">
+            <div className="column">
+              <Image src="/vinyl_record.png" alt="vinyl record" width="50" height="50" />
+            </div>
+            <div className="column">
+                <div className="text">
+                    <b>CURRENTLY LISTENING <br />
+                    TO VINYL</b>
+                </div>
+            </div>
+
+            <style jsx>{`
+            .container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            border: 1px solid silver;
+            border-radius: .5rem;
+            max-width: 20rem;
+            }  
+            .column {
+            display: flex;
+            margin: .5rem 1rem .5rem .5rem;
+            }
+            .image-column {
+            display: flex;
+            margin: 1rem 2rem 1rem 1rem;
+            }
+            img {
+            max-width: 5rem;
+            border-radius: .5rem;
+            }
+            .text {
+            font-family: monospace;
+            font-size: .75rem;
+            text-align: left;
+            justify-content: center;
+            align-items: center;
+            margin: 0rem 2rem 0rem 0rem;
+            }
+            `}</style>
+          </div>
+        :<div className="container">
             <div className="column">
                 <img src={nowPlaying.albumImageUrl} />
             </div>
@@ -30,9 +73,9 @@ export default function NowPlayingModule() {
                 </div>
             </div>
             <div className="image-column">
-              <Image src="/Spotify_Icon_RGB_Black 2.png" alt="me" width="30" height="30" />
+              <Image src="/spotify_icon.png" alt="Spotify Logo" width="30" height="30" />
             </div>
-        
+              
             <style jsx>{`
             .container {
             display: flex;
@@ -63,7 +106,8 @@ export default function NowPlayingModule() {
             margin: 0rem 2rem 0rem 0rem;
             }
             `}</style>
-    </div>
+        </div>
+      }
     </center>
   )
 }
