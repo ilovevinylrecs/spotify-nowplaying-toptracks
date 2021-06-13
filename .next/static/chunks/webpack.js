cfbea411a198fc960a493dad6566c16a5521ad4c
Andrew Nystrom <andrew.nystrom@gmail.com>
2021-06-13 18:23:22 -0400

    lints spotify project

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f639111ff0fe098f4bb4";
+/******/ 	var hotCurrentHash = "3878df73f096d18b1533";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
